8fc7d9c33f1c49a92b5e37dd957d18ff49c17630
Ubuntu <ubuntu@ip-172-31-41-90.us-east-2.compute.internal>
2025-10-17 07:06:31 +0000

    update

--- neuronx-distributed-inference/examples/qwen2_vl/model_config.py
+++ neuronx-distributed-inference/examples/qwen2_vl/model_config.py
@@ -1,9 +1,9 @@
-num_img_per_prompt=2 #1 2 4 8
+num_img_per_prompt=128 #1 2 4 8
 messages = [
     {
         "role": "user",
         "content": [
-            {"type": "image", "image": "/home/ubuntu/qwenvl/qwen-vl-main/neuronx-distributed-inference/examples/qwen2_vl/dog.jpg"}
+            {"type": "image", "image": "/home/ubuntu/qwen2-vl/neuronx-distributed-inference/examples/qwen2_vl/dog_640_480.jpg"}
             for _ in range(num_img_per_prompt)
         ] + [
             {"type": "text", "text": "Describe what you see in the images"}
